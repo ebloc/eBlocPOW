b0d99e3d776416c866588fb8441ecd5545389068
eBloc <ebloc@protonmail.com>
2017-11-20 21:27:31 +0300

    update

--- server.sh
+++ server.sh
@@ -9,4 +9,4 @@ sleep 5
 pass_dir=$ebloc_path/pass.js;
 echo $pass_dir
 echo 'loadScript("'$pass_dir'")' | sudo geth --datadir "$ebloc_path" attach ipc:$ebloc_path/geth.ipc console
-echo 'net'                     | sudo geth --datadir "$ebloc_path" attach ipc:$ebloc_path/geth.ipc console
+echo 'net'                       | sudo geth --datadir "$ebloc_path" attach ipc:$ebloc_path/geth.ipc console
