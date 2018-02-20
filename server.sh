013b7abab451241a6d27e4a22b4b7fcc65f0cada
eBloc <ebloc@protonmail.com>
2018-02-20 16:21:31 +0300

    update.

--- server.sh
+++ server.sh
@@ -2,8 +2,9 @@
 
 sudo killall geth
 DATADIR="/Users/alper/DENE/eblocGeth"
-nohup geth --datadir $DATADIR/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal &
+nohup geth --datadir $DATADIR/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal > gethServer.out &
 
+echo "Please wait few seconds for geth-server to be activated."
 sleep 6
 
 echo "loadScript(\"$DATADIR"/pass.js"\")" | sudo geth --datadir "$DATADIR/private" attach ipc:$DATADIR/private/geth.ipc console
