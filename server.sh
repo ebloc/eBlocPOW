c726400c87cc9ecd00081dcbf84e0ce613d175fb
eBloc <ebloc@protonmail.com>
2018-04-03 12:03:47 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+# ensure running as root
+if [ "$(id -u)" != "0" ]; then
+  echo "sudo: Please run: sudo bash server.sh";
+  exit
+fi
+
 sudo killall geth
 DATADIR="/Users/alper/DENE/eblocGeth"
 nohup geth --datadir $DATADIR/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal > gethServer.out &
