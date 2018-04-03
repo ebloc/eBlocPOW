d097e56829e0c50f721b517342d8077d3125ce45
eBloc <ebloc@protonmail.com>
2018-04-03 12:10:40 +0300

    Update server.sh

--- server.sh
+++ server.sh
@@ -8,7 +8,7 @@ fi
 
 sudo killall geth
 DATADIR=""
-nohup geth --datadir $DATADIR/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal > gethServer.out &
+nohup geth --fast --cache=1024 --datadir $DATADIR/private --port 3000 --rpcaddr 127.0.0.1 --rpc --rpcport 8545 --rpccorsdomain="*" --networkid 23422 --rpcapi eth,net,web3,personal > gethServer.out &
 
 echo "Please wait few seconds for geth-server to be activated."
 sleep 6
